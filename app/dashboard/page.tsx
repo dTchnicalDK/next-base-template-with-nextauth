d7942e5f40c49c341fb36223ab04d2602a832855
dharmendra <dtechnicaldk@gmail.com>
2026-03-23 15:40:54 +0530

    now user is being displayed on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,29 @@
+import { auth } from "@/auth";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const DashboardPage = () => {
-  return <div>Dashboard page</div>;
+const DashboardPage = async () => {
+  const session = await auth();
+  // console.log("session", session);
+  if (!session) redirect("/login");
+  return (
+    <div>
+      Dashboard page
+      <div>
+        <div>
+          <Image
+            src={session.user?.image ?? ""}
+            alt="usrPhoto"
+            width={200}
+            height={250}
+            className="rounded-full overflow-hidden"
+          />
+        </div>
+        <h1>welcome Mr. {session.user?.name}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
