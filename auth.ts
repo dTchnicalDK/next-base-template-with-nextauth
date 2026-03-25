83ce95a15e3df84684ce1e2baa3d4824a3dc9fcc
dharmendra <dtechnicaldk@gmail.com>
2026-03-25 12:43:16 +0530

    bug fix

--- auth.ts
+++ auth.ts
@@ -3,6 +3,16 @@ import { prisma } from "@/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import bcrypt from "bcryptjs";
+
+// Dummy user (replace with DB later)
+const users = [
+  {
+    id: "1",
+    email: "test@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -14,12 +24,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
+
+    //implementing credential login
   ],
   adapter: PrismaAdapter(prisma),
   callbacks: {
     async session({ session, user }) {
       session.user.id = user.id; // now available as session.user.id
-      console.log("session", session);
+      // console.log("session", session);
       return session;
     },
   },
